a7cd5ea2c8b86673a70642c2e35f2a45f5df05a2
mirror-kt <me@mirror-kt.dev>
2022-10-11 23:43:43 +0900

    ✨ Add maven central mirror settings

---
+++ dot_gradle/init.d/mirror-maven-central.init.gradle.kts
@@ -0,0 +1,8 @@
+allprojects {
+    repositories {
+        maven("https://maven-central-asia.storage-download.googleapis.com/maven2/") {
+            name = "Maven Central mirror"
+        }
+    }
+
+}
