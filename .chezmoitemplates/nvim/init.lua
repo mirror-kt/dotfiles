28930e4b51f9877de3a2efb87ff477e741a0c11a
mirror-kt <me@mirror-kt.dev>
2022-10-12 22:17:58 +0900

    ✨ Add nvim config

---
+++ .chezmoitemplates/nvim/init.lua
@@ -0,0 +1,15 @@
+-- locale
+vim.opt.fenc = "utf-8"
+vim.opt.autoread = true
+
+vim.opt.number = true
+vim.opt.cursorline = true
+-- vim.opt.cursorcolumn = true
+vim.opt.smartindent = true
+vim.opt.showmatch = true
+
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
