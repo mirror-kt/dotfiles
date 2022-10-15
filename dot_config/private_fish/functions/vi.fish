687457efdc99b92959375d957e930f31b40bef44
mirror-kt <me@mirror-kt.dev>
2022-10-15 11:09:07 +0900

    🔧 Use nvim instead of vim

--- dot_config/private_fish/functions/vi.fish
+++ dot_config/private_fish/functions/vi.fish
@@ -1,3 +1,7 @@
-function vi --wraps=vim --description 'alias vi vim'
-  vim $argv; 
+function vi --wraps=vim --description 'alias vi nvim'
+  nvim $argv; 
+end
+
+function vim --wraps=nvim --description 'alias vim nvim'
+  nvim $args;
 end
