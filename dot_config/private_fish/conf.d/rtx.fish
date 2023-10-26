0cf048cd13e59985e0c3573b988bcecaaba2c06b
mirror-kt <me@mirror-kt.dev>
2023-10-26 23:24:31 +0900

    🍻 Update somethings

--- dot_config/private_fish/conf.d/rtx.fish
+++ dot_config/private_fish/conf.d/rtx.fish
@@ -1,5 +1,2 @@
 # Activate rtx
 rtx activate fish | source
-
-# Add yarn global installs to $PATH
-set -x PATH $PATH:(rtx x nodejs@latest -- yarn global bin)
--- dot_gradle/init.d/mirror-maven-central.init.gradle.kts
+++
@@ -1,8 +0,0 @@
-allprojects {
-    repositories {
-        maven("https://maven-central-asia.storage-download.googleapis.com/maven2/") {
-            name = "Maven Central mirror"
-        }
-    }
-
-}
